94c31929070b4f60016e7e899b144f9144eb8d57
Ilya <godfrid7@mail.ru>
2022-11-21 23:15:23 +0300

    src/main.java

---
+++ src/main.java
@@ -0,0 +1,21 @@
+public class main {
+    public static void main(String[] args) {
+
+        int bonus;
+        int client = 200;
+
+        if (client > 1000) {
+            System.out.println(bonus = 1);
+        } else {
+            System.out.println(client);
+        }
+
+         client = 1200;
+        if (client > 1100) {
+            System.out.println(client + 11);
+        } else {
+            System.out.println(client);
+
+        }
+    }
+}
